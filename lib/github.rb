df6debe9da9056d5f2cd06ec412cdcce14e1bf61
Pablo Carranza <pcarranza@gmail.com>
2015-06-28 19:07:40 -0400

    Extracted contributions and added commits planner, so far it calculates the commit ranges

--- lib/github.rb
+++ lib/github.rb
@@ -1,16 +1,51 @@
 require "faraday"
 
 class Github
-  attr_reader :contributions, :max_contribution
   def initialize(**options)
     @contributions_fetcher = options.fetch(:fetcher) do
       ContributionsFetcher.new(username: options.fetch(:username))
     end
+    @contributions = options[:contributions]
   end
 
   def fetch_contributions
-    @contributions = @contributions_fetcher.fetch
-    @max_contribution = @contributions.max_by(&:count)
+    @contributions ||= @contributions_fetcher.fetch
+  end
+end
+
+class Contributions
+  def initialize(**options)
+    @contributions = options.fetch(:contributions) do
+      options.fetch(:github).fetch_contributions
+    end
+  end
+
+  def max
+    @max ||= @contributions.max_by(&:count)
+  end
+
+  def each
+    @contributions.each do |contribution|
+      yield contribution
+    end
+  end
+end
+
+class CommitsPlanning
+  def initialize(**options)
+    @contributions = Contributions.new(options)
+  end
+
+  def commit_ranges
+    return @commit_ranges if @commit_ranges
+    max_contribution = @contributions.max.count + 1
+    max_contribution = 10 * (max_contribution.to_f / (10 ** (Math.log10(max_contribution).ceil - 1))).ceil if max_contribution > 10
+    @commit_ranges = (0..4).map { |index| [index, (max_contribution) * index] }.to_h
+  end
+
+  private
+  def baseline
+
   end
 end
 
@@ -55,5 +90,11 @@ class ContributionsParser
     html.to_enum(:scan, @pattern).map { Regexp.last_match }.map do |values|
       Contribution.new(values[:count].to_i, values[:date])
     end
+  end
+end
+
+class CommitPlanner
+  def initialize(github: github)
+    @github = github
   end
 end
--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -17,14 +17,8 @@ describe "Github" do
 
   it "fetches contributions" do
     github = Github.new(username: "pcarranza", fetcher: fetcher)
-    github.fetch_contributions
-    expect(github.contributions).to eq contributions
-  end
-
-  it "picks the maximun contribution" do
-    github = Github.new(username: "pcarranza", fetcher: fetcher)
-    github.fetch_contributions
-    expect(github.max_contribution).to eq Contribution.new(1, "2014-06-12")
+    fetched_contributions = github.fetch_contributions
+    expect(fetched_contributions).to eq contributions
   end
 
   it "fails without a username or fetcher" do
@@ -47,4 +41,55 @@ describe "ContributionsFetcher" do
     expect(contributions[11]).to eq(Contribution.new(7, "2014-07-08"))
     expect(contributions[15]).to eq(Contribution.new(4, "2014-07-12"))
   end
+end
+
+describe "Contributions" do
+  it "picks the maximun contribution from all" do
+    contributions = Contributions.new(contributions: [
+                                      Contribution.new(0, "2014-06-11"),
+                                      Contribution.new(7, "2014-06-12"),
+                                      Contribution.new(0, "2014-06-13"),
+                                      Contribution.new(4, "2014-06-14")])
+    expect(contributions.max).to eq Contribution.new(7, "2014-06-12")
+  end
+end
+
+describe "CommitsPlanning" do
+  let(:contributions) do
+    [Contribution.new(0, "2014-06-01"),
+      Contribution.new(9, "2014-06-02"),
+      Contribution.new(4, "2014-06-03"),
+      Contribution.new(1, "2014-06-04"),
+      Contribution.new(0, "2014-06-05")]
+  end
+  let(:github) do
+    Github.new(username: "username", contributions: contributions)
+  end
+  let(:commits_planning) do
+    CommitsPlanning.new(contributions: contributions)
+  end
+
+  it "builds with a valid github" do
+    planning = CommitsPlanning.new(github: github)
+    expect(planning).not_to be_nil
+  end
+
+  it "builds with a valid contributions list" do
+    planning = CommitsPlanning.new(contributions: contributions)
+    expect(planning).not_to be_nil
+  end
+
+  it "calculates the commit ranges correctly" do
+    expect(commits_planning.commit_ranges).to eq(0 => 0, 1 => 10, 2 => 20, 3 => 30, 4 => 40)
+  end
+  it "calculates the commit ranges fine with a larger ranges starting on the next tens" do
+    planning = CommitsPlanning.new(contributions: [Contribution.new(23, "2014-06-01")])
+    expect(planning.commit_ranges).to eq(0 => 0, 1 => 30, 2 => 60, 3 => 90, 4 => 120)
+  end
+
+  it "calculates the commit ranges fine with a larger ranges also on the tens" do
+    planning = CommitsPlanning.new(contributions: [Contribution.new(30, "2014-06-01")])
+    expect(planning.commit_ranges).to eq(0 => 0, 1 => 40, 2 => 80, 3 => 120, 4 => 160)
+  end
+
 end
