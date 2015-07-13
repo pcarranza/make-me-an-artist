798bb86d2f9a2be963bf2433dfa61b9f9a116a69
Pablo Carranza <pcarranza@gmail.com>
2015-07-13 12:30:20 -0400

    Commit ranges are calculated correctly now
    The intent here is to commit as less as possible, so, If I have 1 commit
    and my goal is to be on the :low range, I do not need to commit at all.
    
    I needed to add a class to handle the range pair so I can just check if
    we are over or under the minimum required.
    
    There may be some fixes to do when applying this to a real repo, but so
    far this looks ok from the intent point of view, I will review later the
    real need for commits when it is actually being done

--- lib/github.rb
+++ lib/github.rb
@@ -13,42 +13,6 @@ class Github
   end
 end
 
-class Contributions
-  def initialize(**options)
-    @contributions = options.fetch(:contributions) do
-      options.fetch(:github).fetch_contributions
-    end
-  end
-
-  def max
-    @max ||= @contributions.max_by(&:count)
-  end
-
-  def each
-    @contributions.each do |contribution|
-      yield contribution
-    end
-  end
-end
-
-class CommitsPlanning
-  def initialize(**options)
-    @contributions = Contributions.new(options)
-  end
-
-  def commit_ranges
-    return @commit_ranges if @commit_ranges
-    max_contribution = @contributions.max.count + 1
-    max_contribution = 10 * (max_contribution.to_f / (10 ** (Math.log10(max_contribution).ceil - 1))).ceil if max_contribution > 10
-    @commit_ranges = (0..4).map { |index| [index, (max_contribution) * index] }.to_h
-  end
-
-  private
-  def baseline
-
-  end
-end
-
 class ContributionsFetcher
   def initialize(**options)
     @connection = options.fetch(:connection) do
@@ -90,5 +54,103 @@ class ContributionsParser
     html.to_enum(:scan, @pattern).map { Regexp.last_match }.map do |values|
       Contribution.new(values[:count].to_i, values[:date])
     end
+  end
+end
+
+class Contributions
+  include Enumerable
+
+  def initialize(**options)
+    @contributions = options.fetch(:contributions) do
+      options.fetch(:github).fetch_contributions
+    end
+  end
+
+  def max
+    @max ||= @contributions.max_by(&:count)
+  end
+
+  def baseline_commits
+    baseline = max.count + 1
+    baseline = 10 * (baseline.to_f / (10 ** (Math.log10(baseline).ceil - 1))).ceil if baseline > 10
+    baseline = 10 if baseline < 10
+    baseline
+  end
+end
+
+class CommitRanges
+
+  RANGE_NAMES = [:zero, :low, :mid, :high, :max]
+
+  def initialize(**options)
+    baseline = Contributions.new(options).baseline_commits
+    @commit_ranges = RANGE_NAMES.each_with_index.map {|range_name, index|
+      [range_name, (baseline) * index]
+    }.to_h
+    @one_less_map = RANGE_NAMES.each_with_index.map {|range_name, index|
+      [range_name, RANGE_NAMES[index - 1]]
+    }.to_h
+    @one_less_map.delete(:zero)
+  end
+
+  def [](range_name)
+    @commit_ranges[range_name]
+  end
+
+  def from(commit_count)
+    RangeFinder.new(self, commit_count)
+  end
+
+  def one_less_than(range)
+    @one_less_map[range]
+  end
+
+  def range_pair(target_range)
+    min_range = one_less_than(target_range)
+    RangePair.new(min_range, target_range)
+  end
+
+  def to_h
+    RANGE_NAMES.each_with_index.map {|range_name, index|
+      [index, @commit_ranges[range_name]]
+    }.to_h
+  end
+end
+
+class RangePair
+  attr_reader :low, :high
+  def initialize(low, high)
+    @low, @high = low, high
+  end
+
+  def ==(other)
+    other.class == self.class && other.state == self.state
+  end
+
+  protected
+
+  def state
+    [low, high]
+  end
+end
+
+class RangeFinder
+  def initialize(ranges, commit_count)
+    @ranges = ranges
+    @commit_count = commit_count
+  end
+
+  def bump_to(target_range)
+    pair = @ranges.range_pair(target_range)
+    return 0 if in_current_range?(pair)
+    minimum_required(pair)
+  end
+
+  def in_current_range?(pair)
+    @commit_count > @ranges[pair.low] && @commit_count < @ranges[pair.high]
+  end
+
+  def minimum_required(pair)
+    @ranges[pair.low] + 1 - @commit_count
   end
 end
--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -52,44 +52,86 @@ describe "Contributions" do
                                       Contribution.new(4, "2014-06-14")])
     expect(contributions.max).to eq Contribution.new(7, "2014-06-12")
   end
+
+  it "calculates the baseline as 10 when the max contribution is 0" do
+    contributions = Contributions.new(contributions: [Contribution.new(0, "2014-06-11")])
+    expect(contributions.baseline_commits).to eq(10)
+  end
+  it "calculates the baseline as 10 when the max contribution is 1" do
+    contributions = Contributions.new(contributions: [Contribution.new(1, "2014-06-11")])
+    expect(contributions.baseline_commits).to eq(10)
+  end
+  it "calculates the baseline as 20 when the max contribution is 12" do
+    contributions = Contributions.new(contributions: [Contribution.new(12, "2014-06-11")])
+    expect(contributions.baseline_commits).to eq(20)
+  end
 end
 
-describe "CommitsPlanning" do
+describe "CommitRanges" do
   let(:contributions) do
-    [Contribution.new(0, "2014-06-01"),
+    [
+      Contribution.new(0, "2014-06-01"),
       Contribution.new(9, "2014-06-02"),
       Contribution.new(4, "2014-06-03"),
       Contribution.new(1, "2014-06-04"),
       Contribution.new(0, "2014-06-05")]
   end
-  let(:github) do
-    Github.new(username: "username", contributions: contributions)
-  end
-  let(:commits_planning) do
-    CommitsPlanning.new(contributions: contributions)
+
+  it "calculates the basic case correctly" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect([ranges[:zero], ranges[:low], ranges[:mid], ranges[:high], ranges[:max]]).to eq(
+      [0, 10, 20, 30, 40])
   end
 
-  it "builds with a valid github" do
-    planning = CommitsPlanning.new(github: github)
-    expect(planning).not_to be_nil
-  end
-
-  it "builds with a valid contributions list" do
-    planning = CommitsPlanning.new(contributions: contributions)
-    expect(planning).not_to be_nil
-  end
-
-  it "calculates the commit ranges correctly" do
-    expect(commits_planning.commit_ranges).to eq(0 => 0, 1 => 10, 2 => 20, 3 => 30, 4 => 40)
-  end
   it "calculates the commit ranges fine with a larger ranges starting on the next tens" do
-    planning = CommitsPlanning.new(contributions: [Contribution.new(23, "2014-06-01")])
-    expect(planning.commit_ranges).to eq(0 => 0, 1 => 30, 2 => 60, 3 => 90, 4 => 120)
+    ranges = CommitRanges.new(contributions: [Contribution.new(23, "2014-06-01")])
+    expect(ranges.to_h).to eq(0 => 0, 1 => 30, 2 => 60, 3 => 90, 4 => 120)
   end
 
   it "calculates the commit ranges fine with a larger ranges also on the tens" do
-    planning = CommitsPlanning.new(contributions: [Contribution.new(30, "2014-06-01")])
-    expect(planning.commit_ranges).to eq(0 => 0, 1 => 40, 2 => 80, 3 => 120, 4 => 160)
+    ranges = CommitRanges.new(contributions: [Contribution.new(30, "2014-06-01")])
+    expect(ranges.to_h).to eq(0 => 0, 1 => 40, 2 => 80, 3 => 120, 4 => 160)
   end
 
+  it "Picks the right range pair" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.range_pair(:low)).to eq(RangePair.new(:zero, :low))
+    expect(ranges.range_pair(:mid)).to eq(RangePair.new(:low, :mid))
+    expect(ranges.range_pair(:high)).to eq(RangePair.new(:mid, :high))
+    expect(ranges.range_pair(:max)).to eq(RangePair.new(:high, :max))
+  end
+
+  it "finds that 1 commit is needed to bump to low from 0" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(0).bump_to(:low)).to eq(1)
+  end
+
+  it "finds that 11 commits are needed to bump to mid from 0" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(0).bump_to(:mid)).to eq(11)
+  end
+
+  it "finds that 21 commits are needed to bump to mid from 0" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(0).bump_to(:high)).to eq(21)
+  end
+
+  it "finds that 31 commits are needed to bump to mid from 0" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(0).bump_to(:max)).to eq(31)
+  end
+
+  it "finds that no commit is needed to bump to low from 1" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(1).bump_to(:low)).to eq(0)
+  end
+
+  it "finds that 10 commits are needed to bump to mid from 1" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(1).bump_to(:mid)).to eq(10)
+  end
+  it "finds that no commits are needed to bump to max from max" do
+    ranges = CommitRanges.new(contributions: contributions)
+    expect(ranges.from(31).bump_to(:max)).to eq(0)
+  end
 end
